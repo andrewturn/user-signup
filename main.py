ad2b5aa17848e1834f5272d829aef6b6b654322e
Andrew Turn <arturn63021@yahoo.com>
2017-02-06 20:06:35 -0600

    finished user-signup

--- main.py
+++ main.py
@@ -15,72 +15,91 @@
 # limitations under the License.
 #
 import webapp2
+import cgi
+import re
+import os
+import jinja2
 
-page_header = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>User Signup</title>
-        <style>
-                .error {
-                    color: red;
-                }
-        </style>
-    </head>
-    <body>
-        <h1>Signup</h1>
-        <form method="post">
-            <table>
-                <tbody>
-                    <tr>
-                        <td>
-                            <label for="username">Username</label>
-                        </td>
-                        <td>
-                            <input name="username" type="text" value required>
-                            <span class="error"</span>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="password" >Password</label>
-                        </td>
-                        <td>
-                            <input name="password" type="password" required>
-                            <span> class="error"</span>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="verify">Verify Password</label>
-                        </td>
-                        <td>
-                            <input name="verify" type="password" required>
-                            <span> class="error"</span>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="email">Email (optional)</lable>
-                        </td>
-                        <td>
-                            <input name="email" type="email" value>
-                            <span> class="error"</span>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-            <input type="submit">
-        </form>
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                                autoescape = True)
 
-    </body>
-</html>
-"""
+def render_str(template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
 
-class MainHandler(webapp2.RequestHandler):
+class BaseHandler(webapp2.RequestHandler):
+    def render(self, templates, **kw):
+        self.response.out.write(render_str(template, **kw))
+
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+
+
+
+
+# class ErrorHandler(webapp2.RequestHandler):
+
+
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def user_valid(username):
+    return username and USER_RE.match(username)
+
+
+PASS_RE = re.compile(r"^.{3,20}$")
+def password_valid(password):
+    return password and PASS_RE.match(password)
+
+EMAIL_RE = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+def email_valid(email):
+    return not email or EMAIL_RE.match(email)
+
+class Signup(BaseHandler):
     def get(self):
-        self.response.write(page_header)
+        self.render("signup-form.html")
+
+    def post(self):
+        have_error = False
+        username = self.request.get('username')
+        password = self.request.get('password')
+        verify = self.request.get('verify')
+        email = self.request.get('email')
+
+        params = dict(username = username,
+                      email = email)
+
+        if not user_valid(username):
+            params['error_username'] = "Not a valid username."
+            have_error = True
+
+        if not password_valid(password):
+            params['error_password'] = "Not a valid password."
+            have_error = True
+        elif password != verify:
+            params['error_verify'] = "Passwords did not match."
+            have_error = True
+
+        if not email_valid(email):
+            params['error_email'] = "That's not a valid email."
+            have_error = True
+
+        if have_error:
+            self.render('signup-form.html', **params)
+        else:
+            self.redirect('/welcome?username= ' + username)
+
+class Welcome(BaseHandler):
+    def get(self):
+        username = self.request.get('username')
+        self.response.write('Welcome,' + username)
+        # if user_valid(username):
+        #     self.render('welcome.html', username=username)
+        # else:
+        #     self.redirect('/signup')
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+    ('/', Signup),
+    ('/welcome', Welcome),
+    ], debug=True)
